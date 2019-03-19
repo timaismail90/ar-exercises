672b001f039f0c3b11ab58a37f460a3997984247
fatuma ismail <timaismail90@gmail.com>
2019-03-19 17:28:49 +0000

    exercise_2.rb : id on the stores updated

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+stores1 = Store.find_by(id: '1')
+stores1.update(id: '@store1')
+
+stores2 = Store.find_by(id: '2')
+stores2.update(id: '@store2')
+
