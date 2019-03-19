986c57877961dfff787b3d9897a795b5e26f1573
fatuma ismail <timaismail90@gmail.com>
2019-03-19 18:39:05 +0000

    done exercise 4 - the first 2 parts. used 'where' and find_each to find the subset of stores that sell men's apperal. I then looped through them and printed out their names and annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+stores4 = Store.create(name: "Surrey", annual_revenue: "224000", mens_apparel:false, womens_apparel: true)
+stores5 = Store.create(name:"Whistler", annual_revenue: "1900000", mens_apparel: true, womens_apparel: false)
+stores6= Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+Store.where(mens_apparel: true).find_each do |store|
+    puts "#{store.name}, #{store.annual_revenue}"
+end
+
+
+
