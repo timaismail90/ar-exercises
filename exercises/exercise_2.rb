0b905c4934e0bfa495dbf56c340f341abe0d7930
fatuma ismail <timaismail90@gmail.com>
2019-03-19 17:38:40 +0000

    corrected the assigning of the instance variable

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,9 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-stores1 = Store.find_by(id: '1')
-stores1.update(id: '@store1')
+@store1= Store.find_by(id: '1')
+@store1.update(name: 'Hamad\'s Abayas')
 
-stores2 = Store.find_by(id: '2')
-stores2.update(id: '@store2')
+@store2 = Store.find_by(id: '2')
 
